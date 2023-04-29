79c26405f1fa3413d451372041e8e622af366606
EJLee1209 <dldmswo1209@gmail.com>
2023-04-29 14:14:30 +0900

    Initial Commit

---
+++ MemoApp_TCA/MemoApp_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp_TCAApp.swift
+//  MemoApp_TCA
+//
+//  Created by 이은재 on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
